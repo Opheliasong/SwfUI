9fe2a246fdb2f265f81e9106a4af1823b160935a
SungMin Park <ophelia.song00@gmail.com>
2021-12-07 12:06:39 +0900

    Initial Commit

---
+++ SwfUI/SwfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfUIApp.swift
+//  SwfUI
+//
+//  Created by Sung Min Park on 2021/12/07.
+//
+
+import SwiftUI
+
+@main
+struct SwfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwfUIUITests/SwfUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwfUIUITestsLaunchTests.swift
+//  SwfUIUITests
+//
+//  Created by Sung Min Park on 2021/12/07.
+//
+
+import XCTest
+
+class SwfUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
